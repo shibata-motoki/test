b9fea7770eaf9292fc1b2bde845a8f6b9d84298a
shibata-motoki <motoki.shibata@nex.nikkei.com>
2018-06-21 15:36:08 +0900

    hello display

--- unittest.py
+++ unittest.py
@@ -1,11 +1,1 @@
-import unittest
-
-from main import myabs
-
-
-class TestAbs(unittest.TestCase):
-    def test__positive(self):
-        self.assertEqual(myabs(4),4)
-        
-    def test__negative(self):
-        self.assertEqual(myabs(-4),4)
+print('Hello')
