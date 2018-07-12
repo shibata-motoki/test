f255e5cc3669fcf4056cd0c02e7c1ce0ed97655b
shibata-motoki <motoki.shibata@nex.nikkei.com>
2018-07-12 15:26:50 +0900

    test

--- test_stream_test.py
+++ test_stream_test.py
@@ -1,4 +1,3 @@
-import unittest
 import pytest
 from unittest import mock
 import json
@@ -11,39 +10,35 @@ class TestGetImagedata(unittest.TestCase):
         dummy_image = mock.Mock()
 """
 
-class TestGetImagedata(unittest.TestCase):
-    def test_get_image(self):
-        dummy_image = mock.Mock()
-        dummy_image.top_data = {'hits': [{
-          'images': [{
-              'credit': '',
-              'expires_at': null,
-              'format': 'jpg',
-              'height': 1564,
-              'image_id': 'DSXMZO3270807006072018000001',
-              'src_image_url': 'https://imgix-proxy.n8s.jp/DSXMZO3270807006072018000001-2.jpg',
-              'width': 1564
+def test_get_image(self):
+    dummy_image = mock.Mock()
+    dummy_image.top_data = {'hits': [{
+         'images': [{
+             'credit': '',
+             'expires_at': null,
+             'format': 'jpg',
+             'height': 1564,
+             'image_id': 'DSXMZO3270807006072018000001',
+             'src_image_url': 'https://imgix-proxy.n8s.jp/DSXMZO3270807006072018000001-2.jpg',
+             'width': 1564
+             }]}]}
+    assert get_image(dummy_data) == {'hits': [{'thumbnail':{
+        'credit': '',
+        'format': 'jpg',
+        'height': 1564,
+        'image_id': 'DSXMZO3270807006072018000001',
+        'revision': a['images'][0]['revision'],
+        'src_image_url': 'https://imgix-proxy.n8s.jp/DSXMZO3270807006072018000001-2.jpg',
+        'width': 1564
+        }}][{
+         'images': [{
+             'credit': '',
+             'format': 'jpg',
+             'height': 1564,
+             'image_id': 'DSXMZO3270807006072018000001',
+             'src_image_url': 'https://imgix-proxy.n8s.jp/DSXMZO3270807006072018000001-2.jpg',
+             'width': 1564
               }]}]}
-        assert get_image(dummy_data) == {'hits': [{'thumbnail':{
-            'credit': '',
-            'format': 'jpg',
-            'height': 1564,
-            'image_id': 'DSXMZO3270807006072018000001',
-            'revision': a['images'][0]['revision'],
-            'src_image_url': 'https://imgix-proxy.n8s.jp/DSXMZO3270807006072018000001-2.jpg',
-            'width': 1564
-            }}][{
-          'images': [{
-              'credit': '',
-              'format': 'jpg',
-              'height': 1564,
-              'image_id': 'DSXMZO3270807006072018000001',
-              'src_image_url': 'https://imgix-proxy.n8s.jp/DSXMZO3270807006072018000001-2.jpg',
-              'width': 1564
-              }]}]}
-
-
-
 
 
 
